09103fcdf04c5ccbcff1dec360aae297eea18651
miles <fcmiles@gmail.com>
2014-10-26 19:32:29 -0400

    Added workDays

--- default.aspx.cs
+++ default.aspx.cs
@@ -21,14 +21,20 @@ namespace EmployeeScheduler
                }
           }
           
-
+          /*method on click of the button to get the 2 inputs, and instatiate
+           * the instance of workPeriod.  This instance will initially create
+           * days * shifts number of shifts into a 2d array of shifts.  These
+           * shifts will then be bound to a repeater and posted to screen per corresponding
+           * shift.
+           * 
+           * */
           protected void Button1_Click(object sender, EventArgs e)
           {
                
-               int dayss = Convert.ToInt32(shiftsPerDay.Text.ToString());
-               int shiftsss = Convert.ToInt32(numOfDays.Text.ToString());
-               WorkPeriod myWorkPeriod = (new WorkPeriod(dayss, shiftsss));               
-               Repeater1.DataSource = new[] { myWorkPeriod.workWeek[0, 0] }.ToList();
+               int days = Convert.ToInt32(shiftsPerDay.Text.ToString());
+               int shifts = Convert.ToInt32(numOfDays.Text.ToString());
+               WorkPeriod myWorkPeriod = (new WorkPeriod(days, shifts));               
+               Repeater1.DataSource = new[] { myWorkPeriod.workWeek[0].shiftsInDay[0] }.ToList();
                Repeater1.DataBind();
                Repeater1.Visible = true;
           }
